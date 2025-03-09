3f1bffa55518380f79eb99b4f29c262ef40f8625
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2025-03-09 23:54:54 +0200

    Add app color constants for consistent theming

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF725BA4);
+  static const Color grey = Color(0xFFE9DFFF);
+  static const Color lightGrey = Color(0xFFF5F4F9);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+}
+
+
