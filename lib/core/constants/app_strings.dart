6b26d4907168912e4083011d88ed24890e5e291b
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2025-03-09 23:54:40 +0200

    Add localization support with Arabic and English translations

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,27 @@
+class AppStrings {
+  // App Name
+  static const appName = 'app_name';
+  
+  // Onboarding Screen
+  static const welcomeTitle = 'welcome_title';
+  static const welcomeSubtitle = 'welcome_subtitle';
+  static const getStarted = 'get_started';
+  
+  // Login Screen
+  static const login = 'login';
+  static const enterPhone = 'enter_phone';
+  static const smsCodeInfo = 'sms_code_info';
+  static const next = 'next';
+  
+  // OTP Screen
+  static const otp = 'otp';
+  static const enterOtp = 'enter_otp';
+  static const resendCode = 'resend_code';
+  
+  // Complete Profile Screen
+  static const completeProfile = 'complete_profile';
+  static const username = 'username';
+  static const enterUsername = 'enter_username';
+  static const phoneNumber = 'phone_number';
+  static const changePicture = 'change_picture';
+} 
