4ec24ab660e8994617e1ab3961956d4eebe03ab0
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2025-03-09 23:56:21 +0200

    Add OnboardingScreen widget with gradient background and navigation to login

---
+++ lib/features/onboarding/presentation/screens/onboarding_screen.dart
@@ -0,0 +1,72 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:q_lock/core/widgets/custom_elevated_button.dart';
+
+import '../../../../core/constants/app_images.dart';
+import '../../../../core/constants/app_strings.dart';
+import '../../../../core/routes/app_routes.dart';
+import '../../../../core/widgets/gradient_background.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Image.asset(AppImages.onboarding, fit: BoxFit.contain),
+            ),
+            Expanded(
+              flex: 2,
+              child: GradientBackground(
+                gradientHeight: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        AppStrings.welcomeTitle.tr(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 32.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Text(
+                        AppStrings.welcomeSubtitle.tr(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 16.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                        ),
+                      ),
+                      const Spacer(),
+                      CustomElevatedButton(
+                        isSingleColor: true,
+                        onTap:
+                            () => Navigator.pushReplacementNamed(
+                              context,
+                              AppRoutes.login,
+                            ),
+                        text: AppStrings.getStarted.tr(),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
