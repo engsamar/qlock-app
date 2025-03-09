e4a1c815803b99ed2cf28468656b73050642e5fe
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2025-03-09 23:56:04 +0200

    Add routing and localization support with ScreenUtil integration

---
+++ lib/core/routes/app_routes.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import '../../features/auth/presentation/screens/complete_profile_screen.dart';
+import '../../features/auth/presentation/screens/login_screen.dart';
+import '../../features/auth/presentation/screens/otp_screen.dart';
+import '../../features/onboarding/presentation/screens/onboarding_screen.dart';
+import '../../features/splash/presentation/screens/splash_screen.dart';
+
+class AppRoutes {
+  static const String splash = '/';
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String otp = '/otp';
+  static const String completeProfile = '/complete-profile';
+
+  static Route<dynamic> onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case splash:
+        return MaterialPageRoute(builder: (_) => const SplashScreen());
+      case onboarding:
+        return MaterialPageRoute(builder: (_) => const OnboardingScreen());
+      case login:
+        return MaterialPageRoute(builder: (_) => const LoginScreen());
+      case otp:
+        final args = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(
+          builder: (_) => OTPScreen(phoneNumber: args['phoneNumber']),
+        );
+      case completeProfile:
+        final args = settings.arguments as Map<String, dynamic>;
+        return MaterialPageRoute(
+          builder: (_) => CompleteProfileScreen(phoneNumber: args['phoneNumber']),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => const Scaffold(
+            body: Center(child: Text('Route not found!')),
+          ),
+        );
+    }
+  }
+} 
