b3b21785ba9f19f5e8bb0e6c4a33d9d838d5b9f2
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2025-03-14 16:53:23 +0200

    Add translations for Arabic and English languages, including new strings for chats, profile, settings, and error messages. Update AppStrings constants to reflect these changes.

--- lib/core/constants/app_strings.dart
+++ lib/core/constants/app_strings.dart
@@ -1,27 +1,59 @@
 class AppStrings {
   // App Name
   static const appName = 'app_name';
-  
+
   // Onboarding Screen
   static const welcomeTitle = 'welcome_title';
   static const welcomeSubtitle = 'welcome_subtitle';
   static const getStarted = 'get_started';
-  
+
   // Login Screen
   static const login = 'login';
   static const enterPhone = 'enter_phone';
   static const smsCodeInfo = 'sms_code_info';
   static const next = 'next';
-  
+  static const phoneNumberRequired = 'phone_number_required';
+  static const phoneNumberInvalid = 'phone_number_invalid';
+
   // OTP Screen
   static const otp = 'otp';
   static const enterOtp = 'enter_otp';
   static const resendCode = 'resend_code';
-  
+
   // Complete Profile Screen
   static const completeProfile = 'complete_profile';
   static const username = 'username';
   static const enterUsername = 'enter_username';
   static const phoneNumber = 'phone_number';
   static const changePicture = 'change_picture';
-} 
+
+  // Home Screen
+  static const chats = 'chats';
+  static const profile = 'profile';
+  static const settings = 'settings';
+  static const editProfile = 'edit_profile';
+
+  // Chats Screen
+  static const all = 'all';
+  static const read = 'read';
+  static const unread = 'unread';
+  static const noChatsYet = 'no_chats_yet';
+
+  // Settings Screen
+  static const language = 'language';
+  static const customNotifications = 'custom_notifications';
+  static const muteNotifications = 'mute_notifications';
+  static const inviteFriends = 'invite_friends';
+  static const aboutApp = 'about_app';
+  static const logout = 'logout';
+
+  // network error
+  static const unexpectedError = 'unexpected_error';
+  static const expectedListError = 'expected_list_error';
+  static const expectedMapError = 'expected_map_error';
+  static const requestTimeoutError = 'request_timeout_error';
+  static const checkInternetError = 'check_internet_error';
+  static const serverError = 'server_error';
+  static const requestCancelledError = 'request_cancelled_error';
+  static const unexpectedServerError = 'unexpected_server_error';
+}
