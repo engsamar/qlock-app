80a9815a8e9ccccb35cdf8a2aa94c1193dd19df1
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2025-03-15 13:21:45 +0200

    Remove unused API constants from ApiConstants and clean up AuthInterceptor by eliminating references to these constants in header settings.

--- lib/core/network/api_constants.dart
+++ lib/core/network/api_constants.dart
@@ -1,8 +1,5 @@
 class ApiConstants {
   ApiConstants._();
   static const String baseUrl = 'http://qlock.kodsolutions.net/api/';
-  static const String apiKey = '';
-  static const String platform = '';
-  static const String version = '';
 
 }
--- lib/core/network/interceptors/auth_interceptor.dart
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -2,7 +2,6 @@ import 'package:dio/dio.dart';
 import 'package:q_lock/core/di.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-import '../api_constants.dart';
 import '../api_keys.dart';
 
 class AuthInterceptor extends Interceptor {
@@ -13,9 +12,6 @@ class AuthInterceptor extends Interceptor {
     RequestOptions options,
     RequestInterceptorHandler handler,
   ) async {
-    options.headers[ApiKeys.apiKey] = ApiConstants.apiKey;
-    options.headers[ApiKeys.platformKey] = ApiConstants.platform;
-    options.headers[ApiKeys.versionKey] = ApiConstants.version;
     options.headers[ApiKeys.authorization] =
         'Bearer ${getIt<SharedPreferences>().getString(ApiKeys.bearerToken) ?? ''}';
     handler.next(options);
